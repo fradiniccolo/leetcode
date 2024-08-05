096b8f11bc18e8422d5e3009c23d39e2c3391ca2
Francesco <francescodiniccolo@gmail.com>
2024-08-05 07:13:49 +0100

    ...

--- submissions/Validate Binary Search Tree.py
+++ submissions/Validate Binary Search Tree.py
@@ -10,4 +10,56 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        
+
+        def max_subtree_val(node):
+
+            current_node = node
+            to_visit = [current_node]
+            max_val_found = None
+
+            while to_visit:
+
+                current_node = to_visit.pop(0)
+                if current_node.left:
+                    to_visit.append(current_node.left)
+                if current_node.right:
+                    to_visit.append(current_node.right)
+
+                if max_val_found is None:
+                    max_val_found = current_node.val
+                elif current_node.val > max_val_found:
+                    max_val_found = current_node.val
+
+            return max_val_found
+
+        def min_subtree_val(node):
+
+            current_node = node
+            to_visit = [current_node]
+            min_val_found = None
+
+            while to_visit:
+
+                current_node = to_visit.pop(0)
+                if current_node.left:
+                    to_visit.append(current_node.left)
+                if current_node.right:
+                    to_visit.append(current_node.right)
+
+                if min_val_found is None:
+                    min_val_found = current_node.val
+                elif current_node.val < min_val_found:
+                    min_val_found = current_node.val
+
+            return min_val_found
+
+        def validate_subtree(node):
+            pass
+
+        if min_subtree_val(root.left) >= root.left:
+            return False
+        if max_subtree_val(root.right) <= root.right:
+            return False
+        if not validate_subtree(root):
+            return False
+        return True
