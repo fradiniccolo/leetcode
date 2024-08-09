5e3ad53a46afbcca8b4597b97128b064ab0f15fe
Francesco <francescodiniccolo@gmail.com>
2024-08-09 17:12:30 +0100

    ...

--- submissions/Symmetric Tree.py
+++ submissions/Symmetric Tree.py
@@ -10,22 +10,68 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        
-        current_node= root
-        to_visit = [current_node]
-        nodes_val = []
-        
-        while to_visit:
+        def group_by_row(lst):
+            """returns the original list grouped so that each sublist 
+            represents a level of the binary tree
+            (such lenghts are powers of 2)"""
+            list_lenghts = []
+            while sum(list_lenghts) < len(lst):
+                list_lenghts.append(
+                    2**len(list_lenghts)
+                )
 
+            grouped = []
+
+            while lst:
+                grouped.append([])
+                while len(grouped[-1]) < list_lenghts[0] and lst:
+                    grouped[-1].append(lst.pop(0))
+                list_lenghts.pop(0)
+
+            return grouped
+
+        def is_list_symmetric(lst):
+            """tests if a simple list is symmetric"""
+            if len(lst) > 1:
+                while len(lst) > 1:
+                    if lst[0] != lst[-1]:
+                        return False
+                    lst = lst[1:-1]
+
+            return True
+
+        def is_grouped_list_symmetric(lst):
+            """tests if all the sub-lists are symmetric"""
+            for sublist in lst:
+                if not is_list_symmetric(sublist):
+                    return False
+            return True
+
+        current_node = root
+        to_visit = [current_node]
+        nodes_tuples = []
+
+        while to_visit:
             current_node = to_visit.pop(0)
-            # if not current_node:
-            #     continue
+
+            val = current_node.val
 
             if current_node.left:
+                left = current_node.left.val
                 to_visit.append(current_node.left)
+            else:
+                left = None
+
             if current_node.right:
+                right = current_node.right.val
                 to_visit.append(current_node.right)
-            
-            if not nodes_val:
-                nodes_val.append(current_node)
-            elif 
+            else:
+                right = None
+
+            nodes_tuples.append((val, left, right))
+
+        values = [node_tuple[0] for node_tuple in nodes_tuples]
+
+        grouped_by_row = group_by_row(values)
+
+        return is_grouped_list_symmetric(grouped_by_row)
